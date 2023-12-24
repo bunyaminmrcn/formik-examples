0046ea501910378cad87bf8a25571a17c7c506ff
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-12-24 18:53:34 +0300

    add material ui

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@
 //import RadioGroup from  './examples/radio-group';
 //import DependentFields from  './examples/dependent-fields';
 //import InstantFeedback from  './examples/instant-feedback';
-import ArrayFields from  './examples/array-fields';
+//import ArrayFields from  './examples/array-fields';
+import MaterialUi from  './examples/material-ui';
 
 function App() {
   return (
     <div className="App">
-      <ArrayFields />
+      <MaterialUi />
     </div>
   );
 }
