0628bc873375330487697e81aeac33d6168699af
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-12-24 18:42:50 +0300

    add instant feedback

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 //import Basic from  './examples/basic';
 //import Checkbox from  './examples/checkbox';
 //import RadioGroup from  './examples/radio-group';
-import DependentFields from  './examples/dependent-fields';
+//import DependentFields from  './examples/dependent-fields';
+import InstantFeedback from  './examples/instant-feedback';
 
 function App() {
   return (
     <div className="App">
-      <DependentFields />
+      <InstantFeedback />
     </div>
   );
 }
